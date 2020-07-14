6183f8afbab0d9fffa47cd70eba5a1280076e3e4
Shawn Gluchacki <Shawn@MacBook-Pro-2.local>
2020-07-14 16:52:24 -0600

    initializes server side

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const favicon = require('serve-favicon');
+
+const app = express();
+
+require('dotenv').config();
+require('./config/database');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// API routes
+
+
+
+// Catch all
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`Express app running on port ${port}`)
+});

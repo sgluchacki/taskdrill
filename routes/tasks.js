dc4e58896db2b61fe5424da21ed24096584c01d0
Shawn Gluchacki <Shawn@MacBook-Pro-2.local>
2020-07-21 00:42:52 -0600

    secures server-side routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const tasksCtrl = require('../controllers/tasks');
 
-router.get('/', tasksCtrl.index);
-router.get('/:id', tasksCtrl.indexSubCards);
-router.post('/', tasksCtrl.create);
-// router.post('/:id', tasksCtrl.createSubCard);  May not need this
-router.put('/:id', tasksCtrl.update);
-// router.put('/:parentID/:id', tasksCtrl.updateSubCards);   May not need this
-router.delete('/:id', tasksCtrl.delete);
+router.get('/', checkAuth, tasksCtrl.index);
+router.get('/:id', checkAuth, tasksCtrl.indexSubCards);
+router.post('/', checkAuth, tasksCtrl.create);
+// router.post('/:id', checkAuth, tasksCtrl.createSubCard);  May not need this
+router.put('/:id', checkAuth, tasksCtrl.update);
+// router.put('/:parentID/:id', checkAuth, tasksCtrl.updateSubCards);   May not need this
+router.delete('/:id', checkAuth, tasksCtrl.delete);
 
 function checkAuth(req, res, next) {
     if (req.user) return next();
